c09d0c4d3bec3ff3e5bf9c27d574d61fff4fb650
nazar.slobodian <nazar.slobodian@intive.com>
2022-02-09 13:46:19 +0100

    Add config for PostgreSQL

---
+++ src/typeorm.config.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const typeOrmConfig: TypeOrmModuleOptions = {
+  type: (process.env.DB_TYPE as 'postgres') || 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
+  migrationsRun: false,
+  logging: true,
+  migrationsTableName: 'migration',
+  migrations: [
+    __dirname + '/migration/**/*.ts',
+    __dirname + '/migration/**/*.js',
+  ],
+  synchronize: false,
+  cli: {
+    migrationsDir: 'src/migration',
+  },
+};
+
+export = typeOrmConfig;
